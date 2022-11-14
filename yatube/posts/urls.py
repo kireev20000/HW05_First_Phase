cea214db3d848242119c0623d7ccdeddd47780fb
Alexsandr Kireev <kireev20000@mail.ru>
2022-11-15 01:04:52 +0700

    Первая версия на ревью

---
+++ yatube/posts/urls.py
@@ -0,0 +1,12 @@
+# posts/urls.py
+from django.urls import path
+
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('', views.index),
+    path('group/', views.group_list),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
+]
